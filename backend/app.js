378ddec367da295467283caf1e48753982f20561
ChandraShekar2001 <pvchandu07@gmail.com>
2023-08-20 16:01:14 +0530

    Added landing page to not cause error

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,14 @@ const tableRouter = require('./routes/tableRoutes')
 app.use(cors())
 app.use(express.json())
 
-
 app.use('/api/v1', userRouter);
 app.use('/api/v1', journalRouter);
 app.use('/api/v1', feedRouter);
 app.use('/api/v1', tableRouter);
 
+app.get('/', (req, res) => {
+    res.send("Welcome to the landing page!");
+})
 
 
 module.exports = app;
